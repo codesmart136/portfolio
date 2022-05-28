86a3e348983e73e2f924aeaa24eb39fa4ebc39d7
smartcode1107 <skyb90363@gamil.com>
2022-05-28 11:32:23 +0900

    commit portfolio

---
+++ src/content_option.js
@@ -0,0 +1,174 @@
+const logotext = "PHILIP";
+const meta = {
+    title: "Philip S",
+    description: "I’m Philip.S - 7 years experience in software development",
+};
+
+const introdata = {
+    title: "I’m Philip S",
+    animated: {
+        first: "I love coding",
+        second: "I code cool",
+        third: "Just do it",
+    },
+    description: "Well-qualified software developer familiar with wide range of programming utilities and languages.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "Well-qualified Full-Stack/Blockchain Developer familiar with wide range of programming utilities and languages. Knowledgeable of backend and frontend development requirements. Handles any part of process with ease. Collaborative team player with excellent technical abilities offering 8 years of related experience.",
+};
+// const worktimeline = [{
+//         jobtitle: "Designer of week",
+//         where: "YAdfi",
+//         date: "2020",
+//     },
+//     {
+//         jobtitle: "Designer of week",
+//         where: "Jamalya",
+//         date: "2019",
+//     },
+//     {
+//         jobtitle: "Designer of week",
+//         where: "ALquds",
+//         date: "2019",
+//     },
+// ];
+
+const skills = [{
+        name: "JS frameworks: React, Vue.js, Angular",
+        value: 90,
+    },
+    {
+        name: "JavaScript",
+        value: 85,
+    },
+    {
+        name: "Node.js",
+        value: 90,
+    },
+    {
+        name: "Solidity",
+        value: 85,
+    },
+    {
+        name: "Rust",
+        value: 80,
+    },
+    {
+        name: "Web3.js",
+        value: 90,
+    },
+    {
+        name: "Android/iOS",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "Website development",
+        description: "React, Vue.js, Angular, Node.js, Next.js, Nuxt.js, PHP, and Laravel",
+    },
+    {
+        title: "Blockchain",
+        description: "DeFi, Staking, DEX, NFT marketplance, and NFT minting",
+    },
+    // {
+    //     title: "Wordpress Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+];
+
+const dataportfolio = [{
+        img: "https://blockchain.evacodes.com/images/yop-new.png",
+        desctiption: "Yield Optimization Platform All-in-one yOptimization platform that makes it easy for everyone to access yield markets. Giving you control, custody, insights, and flexibility directly in one single application.",
+        link: "https://yop.finance/",
+    },
+    {
+        img: "https://blockchain.evacodes.com/images/alium.jpeg",
+        desctiption: "NFTs and a decentralized AMM Exchange with a multi-blockchain option.",
+        link: "https://alium.finance/",
+    },
+    {
+        img: "	https://blockchain.evacodes.com/images/nifter.png",
+        desctiption: "Nifter is a music NFT marketplace that allows artists to create, sell and discover unique music.",
+        link: "https://nifter.io/",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "philipstwert@gmail..com",
+    YOUR_FONE: "+(1)503-476-3820",
+    description: "Well-qualified Full-Stack/Blockchain developer familiar with wide range of programming utilities and languages. Knowledgeable of backend and frontend development requirements. Handles any part of process with ease. Collaborative team player with excellent technical abilities offering 8 years of related experience.",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+    // youtube: "https://yo?utube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    // worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
